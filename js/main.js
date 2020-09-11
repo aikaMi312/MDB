1d5b2f58f0cecf2d725cbaed2c82d99bfcb05517
Ainura <miaika4245@gmail.com>
2020-09-11 08:37:33 +0300

    second version

---
+++ js/main.js
@@ -0,0 +1,10 @@
+// function remove(){
+//     let w = this.closest('.formDel'), br = w.nextElementSibling;
+    
+//     if(br.nodeType === 1 && br.nodeName === 'BR')
+//       br.remove();
+    
+//     w.remove();
+//   }
+  
+//   Array.from(document.querySelectorAll('.del')).forEach(b => b.addEventListener('click', remove));
